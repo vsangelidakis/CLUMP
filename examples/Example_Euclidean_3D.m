5120b9c10bc9f0e8f459f643e57f03ec25efd281
Vasileios Angelidakis <v.angelidakis2@newcastle.ac.uk>
2021-08-26 02:50:29 +0100

    Update examples EU and FM

--- examples/Example_Euclidean_3D.m
+++ examples/Example_Euclidean_3D.m
@@ -1,14 +1,14 @@
-%% Script to run clumpGenerator_Euclidean_3D
+%% Script to run GenerateClump_Euclidean_3D
 % 2021 © V. Angelidakis, S. Nadimi, M. Otsubo, S. Utili.
 clc; clear; close all
 addpath(genpath('../functions'))  % Add path to in-house functions
 
 %% Load particle shape from stl file
-inputGeom='ParticleGeometries/Octahedron_Coarse_Mesh.stl';   N=24; rMin=0; div=102; overlap=0.6; output='EU_octaCoarse.txt'; visualise=true;
-% inputGeom='ParticleGeometries/Hexahedron_Coarse_Mesh.stl'; N=24; rMin=0; div=102; overlap=0.6; output='EU_hexaCoarse.txt'; visualise=true;
+% inputGeom='ParticleGeometries/Octahedron_Coarse_Mesh.stl';	N=24; rMin=0; div=102; overlap=0.6; output='EU_octaCoarse.txt'; visualise=true;
+inputGeom='ParticleGeometries/Hexahedron_Coarse_Mesh.stl';	N=21; rMin=0; div=102; overlap=0.6; output='EU_hexaCoarse.txt'; visualise=true;
 
 %% Load particle shape from binary 3D image
-% inputGeom='ParticleGeometries/matFile.mat';   N=24; rMin=0; div=102; overlap=0.6; output='FM_3D_image.txt'; visualise=true;
+% inputGeom='ParticleGeometries/Binary_3D_Cube.mat';   N=21; rMin=0; div=102; overlap=0.6; output='FM_3D_image.txt'; visualise=true;
 
 tic
 [mesh, clump]=GenerateClump_Euclidean_3D( inputGeom, N, rMin, div, overlap, output, visualise ); % true to plot mesh and clump
--- examples/Example_Ferellec_McDowell.m
+++ examples/Example_Ferellec_McDowell.m
@@ -1,4 +1,4 @@
-%% Script to run clumpGenerator_Ferellec_McDowell
+%% Script to run GenerateClump_Ferellec_McDowell
 % 2021 © V. Angelidakis, S. Nadimi, M. Otsubo, S. Utili.
 clc; clear; close all
 addpath(genpath('../functions'))  % Add path to in-house functions
@@ -12,7 +12,7 @@ addpath(genpath('../lib'))  % Add path to in-house functions
 inputGeom='ParticleGeometries/Torus.stl';						dmin=0.1;	rmin=0.01;	rstep=0.01;	pmax=1.0;	seed=5;	output='FM_torus.txt';	visualise=true;
 
 %% Option 2: Load particle shape from binary 3D image
-% inputGeom='ParticleGeometries/matFile.mat';	dmin=0.2;	rmin=0.5;	rstep=0.2;	pmax=1.0;	seed=5;	output='FM_hexaFineMoved.txt';	visualise=true;
+% inputGeom='ParticleGeometries/Binary_3D_Cube.mat';	dmin=0.2;	rmin=0.5;	rstep=0.2;	pmax=1.0;	seed=5;	output='FM_3D_image.txt';	visualise=true;
 
 tic 
 [mesh, clump]=GenerateClump_Ferellec_McDowell( inputGeom, dmin, rmin, rstep, pmax, seed, output, visualise );
