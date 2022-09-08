866ce2c858571417585ae11c73047700e48fa726
Connor-OS <coshaughnessy1@gmail.com>
2022-09-08 15:03:04 +0100

    added example for Euclidean BPM approach

--- examples/Example_Euclidean_3D.m
+++ examples/Example_Euclidean_3D.m
@@ -5,10 +5,10 @@ addpath(genpath('../functions'))  % Add path to in-house functions
 
 %% Load particle shape from stl file
 % inputGeom='ParticleGeometries/Octahedron_Coarse_Mesh.stl';	N=24; rMin=0; div=102; overlap=0.6; output='EU_octaCoarse.txt'; visualise=true;
-inputGeom='ParticleGeometries/Hexahedron_Coarse_Mesh.stl';	N=21; rMin=0; div=102; overlap=0.6; output='EU_hexaCoarse.txt'; visualise=true;
+%inputGeom='ParticleGeometries/Hexahedron_Coarse_Mesh.stl';	N=21; rMin=0; div=102; overlap=0.6; output='EU_hexaCoarse.txt'; visualise=true;
 
 %% Load particle shape from binary 3D image
-% inputGeom='ParticleGeometries/Binary_3D_Cube.mat';   N=21; rMin=0; div=102; overlap=0.6; output='FM_3D_image.txt'; visualise=true;
+inputGeom='ParticleGeometries/Binary_3D_Cube.mat';   N=21; rMin=0; div=102; overlap=0.6; output='FM_3D_image.txt'; visualise=true;
 
 tic
 [mesh, clump]=GenerateClump_Euclidean_3D( inputGeom, N, rMin, div, overlap, output, visualise ); % true to plot mesh and clump
